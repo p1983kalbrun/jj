4173016796f9d199e8087858dabe222ce5989ef2
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2020-04-28 11:58:43 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/jj/JjApplication.java
@@ -0,0 +1,12 @@
+package com.jj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjApplication.class, args);
+	}
+}
